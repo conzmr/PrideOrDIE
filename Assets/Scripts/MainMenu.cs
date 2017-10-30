ecabe3ba457ccbc364fdde547b6166159f33c875
Constanza Madrigal Reyes <madrigalreyes.c@gmail.com>
2017-10-30 15:29:21 -0600

    Initial levels configuration added.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+	public string startLevel;
+	public string levelSelect;
+	public int playerLives;
+	public int playerHealth;
+
+	public void NewGame ()
+	{
+		PlayerPrefs.SetInt ("PlayerCurrentLives", this.playerLives);
+		PlayerPrefs.SetInt ("CurrentPlayerScore", 0);
+		PlayerPrefs.SetInt ("PlayerCurrentHealth", this.playerHealth);
+		PlayerPrefs.SetInt ("PlayerMaxHealth", this.playerHealth);
+		Application.LoadLevel (this.startLevel);
+	}
+
+	public void LevelSelect ()
+	{
+		PlayerPrefs.SetInt ("PlayerCurrentLives", this.playerLives);
+		PlayerPrefs.SetInt ("CurrentPlayerScore", 0);
+		PlayerPrefs.SetInt ("PlayerCurrentHealth", this.playerHealth);
+		PlayerPrefs.SetInt ("PlayerMaxHealth", this.playerHealth);
+		Application.LoadLevel (this.levelSelect);
+	}
+
+	public void QuitGame ()
+	{
+		Application.Quit ();
+	}
+}
