04fcdf771b90bdc936fe0982e8321caf2049dbdb
Constanza Madrigal Reyes <madrigalreyes.c@gmail.com>
2017-11-25 13:43:32 -0600

    Mendez boss

---
+++ Assets/Scripts/MendezController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MendezController : MonoBehaviour 
+{
+	//public string levelToLoad;
+
+	public EnemyMendezController Mendez;
+	public bool alive;
+	private float time;
+
+	// Use this for initialization
+	void Start ()
+	{
+		this.Mendez = FindObjectOfType<EnemyMendezController> ();
+		this.alive = true;
+		time = 2;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+
+		if (this.alive == false) {
+			time -= Time.deltaTime;
+			if (time <= 0) {
+				Application.LoadLevel ("Level Select");
+			}
+		}
+	}
+
+}
